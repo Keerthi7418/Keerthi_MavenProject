2872ea1a825f53a0aaff4c4645cd3b517d306723
Keerthi7418 <149948476+Keerthi7418@users.noreply.github.com>
2024-02-23 18:35:27 +0530

    Add files via upload

---
+++ GradedProjectMaven/src/main/java/com/great/model/Employee.java
@@ -0,0 +1,80 @@
+package com.great.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee 
+{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String employeeName;
+	private String employeeAddress;
+	private String employeePhone;
+	private double employeeSalary;
+
+	public Employee() {}
+
+	public Employee(int id, String employeeName, String employeeAddress, String employeePhone, double employeeSalary) {
+		super();
+		this.id = id;
+		this.employeeName = employeeName;
+		this.employeeAddress = employeeAddress;
+		this.employeePhone = employeePhone;
+		this.employeeSalary = employeeSalary;
+	}
+
+	public Employee(String employeeName, String employeeAddress, String employeePhone, double employeeSalary) {
+		super();
+		
+		this.employeeName = employeeName;
+		this.employeeAddress = employeeAddress;
+		this.employeePhone = employeePhone;
+		this.employeeSalary = employeeSalary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public String getEmployeeAddress() {
+		return employeeAddress;
+	}
+
+	public String getEmployeePhone() {
+		return employeePhone;
+	}
+
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public void setEmployeeAddress(String employeeAddress) {
+		this.employeeAddress = employeeAddress;
+	}
+
+	public void setEmployeePhone(String employeePhone) {
+		this.employeePhone = employeePhone;
+	}
+
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+
+}
